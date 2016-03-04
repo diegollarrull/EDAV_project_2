b6742e092ce1ef8a0b28e2cdea88992844f04471
Diego Llarrull <diego.llarrull@leapsight.com>
2016-03-04 18:00:45 -0500

    final version of the animations code and static files

---
+++ animations/yearly_floods/js/yearly_floods.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#yearly_floods').scianimator({
+	    'images': ['img/yearly_floods1.png', 'img/yearly_floods2.png', 'img/yearly_floods3.png', 'img/yearly_floods4.png', 'img/yearly_floods5.png', 'img/yearly_floods6.png', 'img/yearly_floods7.png', 'img/yearly_floods8.png', 'img/yearly_floods9.png', 'img/yearly_floods10.png', 'img/yearly_floods11.png', 'img/yearly_floods12.png', 'img/yearly_floods13.png', 'img/yearly_floods14.png', 'img/yearly_floods15.png', 'img/yearly_floods16.png', 'img/yearly_floods17.png', 'img/yearly_floods18.png', 'img/yearly_floods19.png', 'img/yearly_floods20.png', 'img/yearly_floods21.png', 'img/yearly_floods22.png', 'img/yearly_floods23.png', 'img/yearly_floods24.png', 'img/yearly_floods25.png', 'img/yearly_floods26.png', 'img/yearly_floods27.png', 'img/yearly_floods28.png', 'img/yearly_floods29.png', 'img/yearly_floods30.png', 'img/yearly_floods31.png', 'img/yearly_floods32.png'],
+	    'width': 1200,
+	    'delay': 1000,
+	    'loopMode': 'loop'
+	});
+	$('#yearly_floods').scianimator('play');
+    });
+})(jQuery);
